bec1f2ea9f353e3b91c175cd89f30c75894fcc3f
mahmud <mahmudhussaininfo@gmail.com>
2025-08-03 20:04:20 +0600

    final url updated

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3131",
-      "https://hotel-booking-kappa-two.vercel.app",
+      "https://hotel-booking-kappa-two.vercel.app/api/v1",
     ],
     credentials: true,
   })
